40e029986a54dbf5a20706cc9e916bd71257330a
Jun <geffrey90@hotmail.com>
2019-03-19 08:17:07 +0800

    added comment with otherwise of map creation

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#br6551",
+	colors := map[string]string{ //declaring map
+		"red":   "#ff0000", //assign key and value pair in map
+		"green": "#br6551", //assign key and value pair in map
 	}
+	//var color map[string]string    //another way to make map with empty value
+	col := make(map[string]string) //another way to make map with empty value
+	col["white"] = "#fffff"
+
+	delete(colors, "red")
+	fmt.Println(col)
 	fmt.Println(colors)
 }
