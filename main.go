819d24f034e3ac8772b26fbbc77085af8ffcad5e
Jun <geffrey90@hotmail.com>
2019-03-19 08:25:53 +0800

    added printMap function

--- main.go
+++ main.go
@@ -6,12 +6,20 @@ func main() {
 	colors := map[string]string{ //declaring map
 		"red":   "#ff0000", //assign key and value pair in map
 		"green": "#br6551", //assign key and value pair in map
+		"white": "#fffff",
 	}
 	//var color map[string]string    //another way to make map with empty value
-	col := make(map[string]string) //another way to make map with empty value
-	col["white"] = "#fffff"
+	// col := make(map[string]string) //another way to make map with empty value
+	// col["white"] = "#fffff" //assign value to map
 
-	delete(colors, "red")
-	fmt.Println(col)
-	fmt.Println(colors)
+	// delete(colors, "red") //delete value from the map
+	// fmt.Println(col)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+
+	for key, value := range c {
+		fmt.Println(key, value)
+	}
 }
